ca92646b97f7d7845657bade29ca5d08bf6cd0fe
Michael Milligan <mmilligan@astro.umn.edu>
2015-06-20 11:57:17 -0500

    add bootstrap, WTForms

--- README.md
+++ README.md
@@ -4,10 +4,21 @@ roaelistr : Make playlists for ROAE on KFAI
 Dependencies
 ------------
 
-flask, flask-bootstrap
+flask, flask-bootstrap, Flask-WTF
 
-The following should suffice:
+requests
 
-	pip install flask
-	pip install flask-bootstrap
-	
+lxml
+
+pandas
+
+In principle, all of these can be installed via pip.
+
+In practice, it is likely much easier to use the system packaged lxml and pandas,
+rather than bother with compliling those libraries their dependencies.
+
+Therefore, in this environment, what we have done is:
+
+	apt-get install python-lxml python-pandas
+	virtualenv --system-site-packages python
+	(python) pip install flask flask-bootstrap Flask-WTF requests
--- roaelistr.py
+++ roaelistr.py
@@ -1,21 +1,49 @@
 from flask import Flask, request, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from flask_wtf.file import FileField
+from wtforms import TextField, TextAreaField, HiddenField, ValidationError, RadioField,\
+BooleanField, SubmitField, IntegerField, FormField, validators
+
 import metallum
+import json
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'roae'
+Bootstrap(app)
 
-@app.route('/', methods=['GET', 'POST'])
+class MainForm(Form):
+	listdata = TextAreaField('Playlist Data')
+	submitted = SubmitField('Process Data')
+
+@app.route('/')
+# Change to serve just static homepage with HTML5 controls
 def starter():
-	if request.method == 'GET':
-		return render_template('input.html')
-	else:
-		tracks = request.form['input']
-		out = []
-		for l in tracks.split('\n'):
-			try:
-				out.append(metallum.getlabel(l)['label'])
-			except:
-				out.append('NOT FOUND')
-		return '<br />\n'.join(out)
+	return render_template('input.html', form=MainForm())
+
+@app.route('/getinfo', methods=['GET','POST'])
+# AJAX route accepts playlist data, does lookups and streams back label data
+def gettrackinfo():
+	tracks = json.loads(request.form['listdata'])
+	out = []
+	for l in tracks:
+		try:
+			out.append(metallum.getalbumdata(l['artist'], l['album']))
+		except:
+			out.append('ERROR')
+	return json.dumps(out)
+
+@app.route('/readtable', methods=['POST'])
+# route for uploading a csv/xls file (whatever Pandas can handle),
+# returns JSON version of table formatted for gettrackinfo
+def readtabfile():
+	return ""
+
+@app.route('/getxls', methods=['GET','POST'])
+# accept gettrackinfo data (possibly edited client-side)
+# return Pandas-generated xls file for upload as playlist
+def getxls():
+	return ""
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
